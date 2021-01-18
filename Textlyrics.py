8ada6cfcfeacfd4b1057e7187005d274aeb51098
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2021-01-18 14:01:49 +0100

    Create Textlyrics.py

---
+++ Textlyrics.py
@@ -0,0 +1,15 @@
+text = 'jag vill sova istället för att gå i skolan.'
+
+test = text[0].islower()
+text1 = text
+
+
+if test == true:
+    text1 = text.replace(text[0], text[0].upper())
+
+if text[-1].endswith ('.') != true:
+    text += '.'
+
+
+print (text1)
+
